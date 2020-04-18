c881339062c01e3265252e4d3966b41df50b9938
gvillegasc <villegasgerardo.j@gmail.com>
2020-04-17 21:01:23 -0500

    Primer commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:header_style/src/pages/headers_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Diseños app',
+        home: HeadersPage());
+  }
+}
---
+++ lib/src/pages/headers_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:header_style/src/widgets/headers.dart';
+
+class HeadersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: HeaderCuadrado());
+  }
+}
