3ed88aaabbef578c2d01c7be6d02870f993ebf25
gvillegasc <villegasgerardo.j@gmail.com>
2020-04-17 21:34:01 -0500

    Añadiendo header con bordes redondeados

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -9,3 +9,17 @@ class HeaderCuadrado extends StatelessWidget {
     );
   }
 }
+
+class HeadersBordesRedondeados extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 300,
+        decoration: BoxDecoration(
+          color: Color(0xff615AAB),
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(50),
+              bottomRight: Radius.circular(50)),
+        ));
+  }
+}
