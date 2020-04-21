b172c21eb861ff8829c672b6a557622a7eee0dbb
gvillegasc <villegasgerardo.j@gmail.com>
2020-04-21 18:29:28 -0500

    Añadiendo Curves en la vista de animaciones

--- lib/src/pages/animaciones_page.dart
+++ lib/src/pages/animaciones_page.dart
@@ -26,12 +26,14 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
   void initState() {
     controller = new AnimationController(
         vsync: this, duration: Duration(milliseconds: 4000));
-    rotacion = Tween(begin: 0.0, end: 2.0 * Math.pi).animate(controller);
+    rotacion = Tween(begin: 0.0, end: 2.0 * Math.pi)
+        .animate(CurvedAnimation(parent: controller, curve: Curves.easeOut));
 
     controller.addListener(() {
-      print('Status: ${controller.status}');
+      // print('Status: ${controller.status}');
       if (controller.status == AnimationStatus.completed) {
-        controller.reverse();
+        // controller.reverse();
+        controller.reset();
       }
     });
     super.initState();
