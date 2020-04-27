1260628ce2969b627de0576bd5e724335bc73dd0
gvillegasc <villegasgerardo.j@gmail.com>
2020-04-27 09:05:42 -0500

    Posicionando menu en la vista de pinterest

--- lib/src/pages/pinterest_page.dart
+++ lib/src/pages/pinterest_page.dart
@@ -6,18 +6,65 @@ class PinterestPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          // child: PinterestGrid(),
-          child: PinterestMenu()),
+        body: Stack(
+      children: <Widget>[
+        PinterestGrid(),
+        _PinterestMenuLocation(),
+      ],
+    ));
+  }
+}
+
+class _PinterestMenuLocation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final widthPantalla = MediaQuery.of(context).size.width;
+
+    return Positioned(
+      bottom: 30,
+      child: Container(
+        width: widthPantalla,
+        child: Align(
+          child: PinterestMenu(),
+        ),
+      ),
     );
   }
 }
 
-class PinterestGrid extends StatelessWidget {
+class PinterestGrid extends StatefulWidget {
+  @override
+  _PinterestGridState createState() => _PinterestGridState();
+}
+
+class _PinterestGridState extends State<PinterestGrid> {
   final List<int> items = List.generate(200, (i) => i);
+  ScrollController controller = new ScrollController();
+  double scrollAnterior = 0;
+
+  @override
+  void initState() {
+    controller.addListener(() {
+      if (controller.offset > scrollAnterior) {
+        print("Ocultar menu");
+      } else {
+        print("Mostrar menu");
+      }
+      scrollAnterior = controller.offset;
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new StaggeredGridView.countBuilder(
+      controller: controller,
       crossAxisCount: 4,
       itemCount: items.length,
       itemBuilder: (BuildContext context, int index) => _PinterestItem(index),
