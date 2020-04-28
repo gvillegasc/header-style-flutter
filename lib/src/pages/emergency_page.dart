3007fab237d9b9e15f1241e671a554de3d664a23
gvillegasc <villegasgerardo.j@gmail.com>
2020-04-27 20:14:43 -0500

    Creando header

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:header_style/src/widgets/headers.dart';
 
 class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-      ),
-    );
+    return Scaffold(body: IconHeader());
   }
 }
--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HeaderCuadrado extends StatelessWidget {
   @override
@@ -210,5 +211,82 @@ class _HeaderWavePainter extends CustomPainter {
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return true;
+  }
+}
+
+class IconHeader extends StatelessWidget {
+  final IconData icon;
+  final String titulo;
+  final String subtitulo;
+  final Colors color1;
+  final Colors color2;
+
+  const IconHeader({
+    @required this.icon,
+    @required this.titulo,
+    @required this.subtitulo,
+    this.color1,
+    this.color2,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final Color colorBlanco = Colors.white.withOpacity(0.7);
+    return Stack(
+      children: <Widget>[
+        _IconHeaderBackground(),
+        Positioned(
+          top: -80,
+          left: -70,
+          child: FaIcon(
+            FontAwesomeIcons.plus,
+            size: 250,
+            color: Colors.white.withOpacity(0.2),
+          ),
+        ),
+        Column(
+          children: <Widget>[
+            SizedBox(
+              height: 80,
+              width: double.infinity,
+            ),
+            Text("Haz solicitado",
+                style: TextStyle(fontSize: 20, color: colorBlanco)),
+            SizedBox(
+              height: 20,
+            ),
+            Text("Asistencia Médica",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: colorBlanco,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 20,
+            ),
+            FaIcon(
+              FontAwesomeIcons.plus,
+              size: 80,
+              color: Colors.white,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _IconHeaderBackground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 300,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(80)),
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: <Color>[Color(0xff526BF6), Color(0xff67ACF2)])),
+    );
   }
 }
